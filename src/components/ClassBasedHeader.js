5d7a90ced6f8ff347bed2cab5f4fc2a5a23c14fd
sydney_okou <sydney.okou@gmail.com>
2022-10-31 21:30:28 +0100

    task tracker project

---
+++ src/components/ClassBasedHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class HeaderClass extends React.Component {
+    render() {
+        return (
+            <header>
+              <h1>Task Tracker Class Based Exemple</h1>
+            </header>
+          )
+    }
+}
+
+export default HeaderClass
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import {useLocation} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            {location.pathname === '/' && <Button
+                color={showAdd ? 'green' : 'red'}
+                text={showAdd ? 'Add' : 'Close'}
+                onClick={onAdd}
+            />}
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title : 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+
+export default Header
